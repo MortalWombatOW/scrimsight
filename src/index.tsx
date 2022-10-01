20664779849d32fe5ddc9fa236e82dea5ea045ec
Andrew Gleeson <andrew.r.gleeson@gmail.com>
2022-10-01 10:46:05 -0700

    f

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ import {setupDB} from './lib/data/database';
 
 setupDB(() => {
   ReactDOM.render(
-    // <React.StrictMode>
-    <App />,
-    // </React.StrictMode>,
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
     document.getElementById('root'),
   );
 });
