358382668221723dd9871a0d816fea5d0e53299f
Andrew Gleeson <mortalwombat@scrimsight.com>
2023-01-01 19:55:13 -0800

    tube L

--- src/components/ThreeRenderer/PlayerConnection.tsx
+++ src/components/ThreeRenderer/PlayerConnection.tsx
@@ -21,8 +21,11 @@ export function PlayerConnection({
     z: number;
   };
 }) {
-  const MAX_POINTS = 10;
+  const MAX_POINTS = 1;
+  const RADIAL_SEGMENTS = 8;
+
   let drawCount = 2;
+
   const midX = (source.x + target.x) / 2;
   const midY = (source.y + target.y) / 2;
   const midZ = (source.z + target.z) / 2;
@@ -36,44 +39,79 @@ export function PlayerConnection({
     new THREE.Vector3(midX, midY + length / 4, midZ),
     new THREE.Vector3(target.x, target.y, target.z),
   );
-  const positions = new Float32Array(MAX_POINTS * 3);
-  const colors = new Float32Array(MAX_POINTS * 3);
+
+  const positions = new Float32Array(MAX_POINTS * RADIAL_SEGMENTS * 3);
+  const colors = new Float32Array(MAX_POINTS * 3 * RADIAL_SEGMENTS);
+  const tubeGeometry = new THREE.TubeGeometry(
+    curve,
+    MAX_POINTS,
+    amount / 20 + 0.5,
+    RADIAL_SEGMENTS,
+    false,
+  );
   const geometry = new THREE.BufferGeometry();
-  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-  geometry.setDrawRange(0, drawCount);
-  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+  geometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, RADIAL_SEGMENTS * 3),
+  );
+  geometry.setDrawRange(0, drawCount * RADIAL_SEGMENTS);
+  geometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, RADIAL_SEGMENTS * 3),
+  );
 
   const color = type === 'damage' ? '#b65153' : '#d7ae0b';
   const threeColor = new THREE.Color(color);
   threeColor.offsetHSL(0, 0, 0);
   console.log('color', color, 'modified', threeColor.getHexString());
-  const material = new THREE.LineBasicMaterial({
-    vertexColors: true,
+  const material = new THREE.MeshPhongMaterial({
+    // vertexColors: true,
+    color: threeColor,
   });
-  const line = new THREE.Line(geometry, material);
-  const DURATION = 0.5;
-  useFrame((_, delta) => {
-    const deltaPercent = delta / DURATION;
-    const deltaPoints = deltaPercent * MAX_POINTS;
-    drawCount = drawCount + deltaPoints;
-    if (drawCount > MAX_POINTS) {
-      drawCount = 0;
-    }
-    if (drawCount > 2) {
-      line.geometry.setDrawRange(0, Math.floor(drawCount));
-    }
-    for (let i = 0; i < drawCount; i++) {
-      const point = curve.getPoint(i / (MAX_POINTS - 1));
-      positions[i * 3] = point.x;
-      positions[i * 3 + 1] = point.y;
-      positions[i * 3 + 2] = point.z;
-      colors[i * 3] = threeColor.r;
-      colors[i * 3 + 1] = threeColor.g;
-      colors[i * 3 + 2] = threeColor.b;
-    }
-    line.geometry.attributes.position.needsUpdate = true;
-    line.geometry.attributes.color.needsUpdate = true;
-  });
+  // tubeGeometry.setDrawRange(1, 3);
+  const mesh = new THREE.Mesh(tubeGeometry, material);
 
-  return <primitive object={line} />;
+  // mesh.geometry.setDrawRange(0, 50);
+  const DURATION = 2;
+  // useFrame((_, delta) => {
+  //   const deltaPercent = delta / DURATION;
+  //   const deltaPoints = deltaPercent * MAX_POINTS;
+  //   drawCount = drawCount + deltaPoints;
+  //   if (drawCount > MAX_POINTS) {
+  //     drawCount = 0;
+  //   }
+  //   if (drawCount > 2) {
+  //     mesh.geometry.setDrawRange(0, Math.floor(drawCount * RADIAL_SEGMENTS));
+  //   }
+  //   for (let i = 0; i < drawCount; i++) {
+  //     const point = tubeGeometry.parameters.path.getPoint(i / (MAX_POINTS - 1));
+  //     const normal = tubeGeometry.parameters.path.getTangent(
+  //       i / (MAX_POINTS - 1),
+  //     );
+  //     const binormal = new THREE.Vector3();
+  //     binormal.crossVectors(normal, new THREE.Vector3(0, 1, 0));
+  //     const matrix = new THREE.Matrix4();
+  //     matrix.lookAt(point, new THREE.Vector3(), normal);
+  //     matrix.multiply(new THREE.Matrix4().makeRotationY(Math.PI / 2));
+  //     matrix.multiply(new THREE.Matrix4().makeRotationZ(Math.PI / 2));
+  //     for (let j = 0; j < RADIAL_SEGMENTS; j++) {
+  //       const angle = (j / RADIAL_SEGMENTS) * Math.PI * 2;
+  //       const x = Math.cos(angle);
+  //       const y = Math.sin(angle);
+  //       const pos = new THREE.Vector3(x, y, 0);
+  //       pos.applyMatrix4(matrix);
+  //       pos.add(point);
+  //       positions[i * RADIAL_SEGMENTS * 3 + j * 3] = pos.x;
+  //       positions[i * RADIAL_SEGMENTS * 3 + j * 3 + 1] = pos.y;
+  //       positions[i * RADIAL_SEGMENTS * 3 + j * 3 + 2] = pos.z;
+  //       colors[i * RADIAL_SEGMENTS * 3 + j * 3] = threeColor.r;
+  //       colors[i * RADIAL_SEGMENTS * 3 + j * 3 + 1] = threeColor.g;
+  //       colors[i * RADIAL_SEGMENTS * 3 + j * 3 + 2] = threeColor.b;
+  //     }
+  //   }
+  //   mesh.geometry.attributes.position.needsUpdate = true;
+  //   mesh.geometry.attributes.color.needsUpdate = true;
+  // });
+
+  return <primitive object={mesh} />;
 }
