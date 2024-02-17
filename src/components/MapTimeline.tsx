3eefaeb275066b732400df7d2103ce5b045b2eb2
Andrew Gleeson <mortalwombat@scrimsight.com>
2024-02-16 21:28:28 -0800

    asdasdf

--- src/components/MapTimeline.tsx
+++ src/components/MapTimeline.tsx
@@ -15,6 +15,8 @@ import {
 } from './Common/RoleIcons';
 import DangerousIcon from '@mui/icons-material/Dangerous';
 import {heroNameToNormalized} from '../lib/string';
+import usePlayerLives from '../hooks/usePlayerLives';
+import useMapTimes from '../hooks/useMapTimes';
 
 const MapTimeline = ({mapId, roundId}: {mapId: number; roundId: number}) => {
   const data = useDataNodes([
@@ -51,55 +53,10 @@ const MapTimeline = ({mapId, roundId}: {mapId: number; roundId: number}) => {
       `,
       ['kill_object_store'],
     ),
-    new AlaSQLNode(
-      'MapTimeline_hero_spawns_' + mapId + '_' + roundId,
-      `SELECT
-        hero_spawn.*
-      FROM ? AS hero_spawn
-      WHERE
-        hero_spawn.mapId = ${mapId}
-      `,
-      ['hero_spawn_object_store'],
-    ),
-    new AlaSQLNode(
-      'MapTimeline_hero_swaps_' + mapId + '_' + roundId,
-      `SELECT 
-        hero_swap.*
-      FROM ? AS hero_swap
-      WHERE
-        hero_swap.mapId = ${mapId}
-      `,
-      ['hero_swap_object_store'],
-    ),
-    new AlaSQLNode(
-      'MapTimeline_mercy_resurrects_' + mapId + '_' + roundId,
-      `SELECT
-        mercy_rez.*
-      FROM ? AS mercy_rez
-      WHERE
-        mercy_rez.mapId = ${mapId}
-      `,
-      ['mercy_rez_object_store'],
-    ),
-    new AlaSQLNode(
-      'MapTimeline_round_end_' + mapId + '_' + roundId,
-      `SELECT
-        round_end.*
-      FROM ? AS round_end
-      WHERE
-        round_end.mapId = ${mapId}
-      `,
-      ['round_end_object_store'],
-    ),
   ]);
 
   const mapRosterRawData = data['MapTimeline_players_' + mapId];
   const kills = data['MapTimeline_kills_' + mapId + '_' + roundId];
-  const heroSpawns = data['MapTimeline_hero_spawns_' + mapId + '_' + roundId];
-  const heroSwaps = data['MapTimeline_hero_swaps_' + mapId + '_' + roundId];
-  const mercyResurrects =
-    data['MapTimeline_mercy_resurrects_' + mapId + '_' + roundId];
-  const roundEnd = data['MapTimeline_round_end_' + mapId + '_' + roundId];
 
   // holds the players for columns
   const [players, setPlayers] = useState<object[]>([]);
@@ -141,8 +98,10 @@ const MapTimeline = ({mapId, roundId}: {mapId: number; roundId: number}) => {
     [name: string]: object[];
   }>({});
 
-  const [startTime, setStartTime] = useState(0);
-  const [endTime, setEndTime] = useState(0);
+  const {startTime, endTime} = useMapTimes(mapId)?.[roundId] || {
+    startTime: 0,
+    endTime: 0,
+  };
   const height = 3000;
   const width = 1000;
   const xPadding = 50;
@@ -173,258 +132,7 @@ const MapTimeline = ({mapId, roundId}: {mapId: number; roundId: number}) => {
 
   // console.log('killsByPlayer', killsByPlayer);
 
-  const [playerLives, setPlayerLives] = useState<{
-    [name: string]: {
-      playerName: string;
-      playerHero: string;
-      startTime: number;
-      endTime: number;
-      startMessage?: string;
-      endMessage?: string;
-    }[];
-  }>({});
-
-  useEffect(() => {
-    if (!kills || !heroSpawns || !heroSwaps || !mercyResurrects || !roundEnd) {
-      return;
-    }
-
-    const startTime_ = Math.min(
-      ...[
-        ...kills,
-        ...heroSpawns,
-        ...heroSwaps,
-        ...mercyResurrects,
-        ...roundEnd,
-      ].map((event) => event.matchTime),
-    );
-    const endTime_ = Math.max(
-      ...[
-        ...kills,
-        ...heroSpawns,
-        ...heroSwaps,
-        ...mercyResurrects,
-        ...roundEnd,
-      ].map((event) => event.matchTime),
-    );
-    setStartTime(startTime_);
-    setEndTime(endTime_);
-    // build playerlives by iterating through events (kills, hero_swaps, hero_spawns, mercy_resurrects) in order
-    // player lifes start with a hero spawn or mercy resurrect, and end with a hero swap or death or round end
-    const playerLives_: {
-      [name: string]: {
-        playerName: string;
-        playerHero: string;
-        startTime: number;
-        endTime: number;
-        startMessage?: string;
-        endMessage?: string;
-      }[];
-    } = {};
-
-    const currentPlayerLives: {
-      [name: string]: {
-        playerName: string;
-        playerHero: string;
-        startTime: number;
-        endTime: number;
-        startMessage?: string;
-        endMessage?: string;
-      };
-    } = {};
-    let killIdx = 0;
-    let heroSpawnIdx = 0;
-    let heroSwapIdx = 0;
-    let mercyResurrectIdx = 0;
-    let roundEndIdx = 0;
-
-    const maxIters = 10000;
-    const iters = 0;
-    while (
-      (killIdx < kills.length ||
-        heroSpawnIdx < heroSpawns.length ||
-        heroSwapIdx < heroSwaps.length ||
-        mercyResurrectIdx < mercyResurrects.length ||
-        roundEndIdx < roundEnd.length) &&
-      iters < maxIters
-    ) {
-      const nextKill = kills[killIdx];
-      const nextHeroSpawn = heroSpawns[heroSpawnIdx];
-      const nextHeroSwap = heroSwaps[heroSwapIdx];
-      const nextMercyResurrect = mercyResurrects[mercyResurrectIdx];
-      const nextRoundEnd = roundEnd[roundEndIdx];
-
-      const nextEvent = [
-        nextKill,
-        nextHeroSpawn,
-        nextHeroSwap,
-        nextMercyResurrect,
-        nextRoundEnd,
-      ]
-        .filter((e) => e)
-        .sort((a, b) => {
-          if (a.matchTime !== b.matchTime) {
-            return a.matchTime - b.matchTime;
-          }
-          // prefer spawn and res
-          if (a.type === 'hero_spawn' || a.type === 'mercy_rez') {
-            return -1;
-          }
-          if (b.type === 'hero_spawn' || b.type === 'mercy_rez') {
-            return 1;
-          }
-          return 0;
-        })[0];
-
-      // console.log(
-      //   'nextEvent',
-      //   nextEvent,
-      //   nextKill,
-      //   nextHeroSpawn,
-      //   nextHeroSwap,
-      //   nextMercyResurrect,
-      //   nextRoundEnd,
-      // );
-      // console.log('currentPlayerLives', currentPlayerLives);
-
-      // check if the last life for each player has ended more than 10 seconds ago. Is so, create a synthetic spawn event and add it to the playerLives_ array
-      const previousLives = Object.keys(playerLives_)
-        .map((playerName) => playerLives_[playerName].slice(-1)[0])
-        .filter(
-          (life) =>
-            nextEvent.matchTime > life.endTime + 10 &&
-            currentPlayerLives[life.playerName] === undefined,
-        );
-
-      for (const life of previousLives) {
-        console.log('synthetic spawn', life);
-        currentPlayerLives[life.playerName] = {
-          playerName: life.playerName,
-          playerHero: life.playerHero,
-          startTime: life.endTime + 10,
-          endTime: 0,
-          startMessage: 'Inferred spawn',
-        };
-      }
-
-      if (nextEvent === nextKill) {
-        if (currentPlayerLives[nextKill.victimName]) {
-          currentPlayerLives[nextKill.victimName].endTime = nextKill.matchTime;
-          currentPlayerLives[nextKill.victimName].endMessage =
-            'Killed by ' + nextKill.attackerName;
-          if (!playerLives_[nextKill.victimName]) {
-            playerLives_[nextKill.victimName] = [];
-          }
-          playerLives_[nextKill.victimName].push(
-            currentPlayerLives[nextKill.victimName],
-          );
-          console.log('kill', currentPlayerLives[nextKill.victimName]);
-          delete currentPlayerLives[nextKill.victimName];
-        } else {
-          console.error('kill with no spawn', nextKill);
-          break;
-        }
-        killIdx++;
-      }
-      if (nextEvent === nextHeroSpawn) {
-        if (currentPlayerLives[nextHeroSpawn.playerName]) {
-          console.error('hero spawn with existing spawn', nextHeroSpawn);
-          break;
-        } else {
-          const previousLife =
-            playerLives_[nextHeroSpawn.playerName]?.slice(-1)?.[0];
-          const startMessage = previousLife ? 'Respawned' : 'Spawned';
-          const startMessageNewHero =
-            nextHeroSpawn.playerHero !== previousLife?.playerHero
-              ? ' as ' + nextHeroSpawn.playerHero
-              : '';
-
-          currentPlayerLives[nextHeroSpawn.playerName] = {
-            playerName: nextHeroSpawn.playerName,
-            playerHero: nextHeroSpawn.playerHero,
-            startTime: nextHeroSpawn.matchTime,
-            endTime: 0,
-            startMessage: startMessage + startMessageNewHero,
-          };
-          console.log(
-            'hero spawn',
-            currentPlayerLives[nextHeroSpawn.playerName],
-          );
-        }
-        heroSpawnIdx++;
-      }
-      if (nextEvent === nextHeroSwap) {
-        if (currentPlayerLives[nextHeroSwap.playerName]) {
-          currentPlayerLives[nextHeroSwap.playerName].endTime =
-            nextHeroSwap.matchTime;
-          if (!playerLives_[nextHeroSwap.playerName]) {
-            playerLives_[nextHeroSwap.playerName] = [];
-          }
-          playerLives_[nextHeroSwap.playerName].push(
-            currentPlayerLives[nextHeroSwap.playerName],
-          );
-          console.log('hero swap', currentPlayerLives[nextHeroSwap.playerName]);
-          delete currentPlayerLives[nextHeroSwap.playerName];
-          currentPlayerLives[nextHeroSwap.playerName] = {
-            playerName: nextHeroSwap.playerName,
-            playerHero: nextHeroSwap.playerHero,
-            startTime: nextHeroSwap.matchTime,
-            endTime: 0,
-            startMessage: 'Swapped to ' + nextHeroSwap.playerHero,
-          };
-        } else {
-          console.error('hero swap with no spawn', nextHeroSwap);
-          // break;
-        }
-        heroSwapIdx++;
-      }
-      if (nextEvent === nextMercyResurrect) {
-        // TODO revivedHero is really revivedName
-        if (currentPlayerLives[nextMercyResurrect.revivedHero] === undefined) {
-          const previousDeath =
-            playerLives_[nextMercyResurrect.revivedHero].slice(-1)[0];
-          currentPlayerLives[nextMercyResurrect.revivedHero] = {
-            playerName: nextMercyResurrect.revivedHero,
-            playerHero: previousDeath.playerHero,
-            startTime: nextMercyResurrect.matchTime,
-            endTime: 0,
-            startMessage: 'Revived by ' + nextMercyResurrect.mercyName,
-          };
-          console.log(
-            'rez',
-            currentPlayerLives[nextMercyResurrect.revivedHero],
-          );
-        } else {
-          console.error('rez with no death', nextMercyResurrect);
-          break;
-        }
-        mercyResurrectIdx++;
-      }
-      if (nextEvent === nextRoundEnd) {
-        for (const playerName in currentPlayerLives) {
-          if (currentPlayerLives[playerName]) {
-            currentPlayerLives[playerName].endTime = nextRoundEnd.matchTime;
-            currentPlayerLives[playerName].endMessage = '';
-            if (!playerLives_[playerName]) {
-              playerLives_[playerName] = [];
-            }
-            playerLives_[playerName].push(currentPlayerLives[playerName]);
-            delete currentPlayerLives[playerName];
-          }
-          console.log('round end', currentPlayerLives[playerName]);
-        }
-        roundEndIdx++;
-      }
-    }
-
-    setPlayerLives(playerLives_);
-  }, [
-    JSON.stringify(kills),
-    JSON.stringify(heroSpawns),
-    JSON.stringify(heroSwaps),
-    JSON.stringify(mercyResurrects),
-    JSON.stringify(roundEnd),
-  ]);
+  const playerLives = usePlayerLives(mapId, roundId);
 
   return (
     <Paper sx={{padding: '1em', borderRadius: '5px', marginTop: '1em'}}>
@@ -460,12 +168,6 @@ const MapTimeline = ({mapId, roundId}: {mapId: number; roundId: number}) => {
           overflowX: 'hidden',
         }}>
         <svg width="100%" height={height}>
-          <circle
-            cx={width / 2}
-            cy={timeToY(startTime, startTime, endTime)}
-            r={5}
-            fill="white"
-          />
           {players.map((player: any, i: number) => (
             <g key={player.playerName}>
               {/* <line
@@ -485,16 +187,36 @@ const MapTimeline = ({mapId, roundId}: {mapId: number; roundId: number}) => {
                       r={5}
                       fill={getColorFor(heroNameToNormalized(life.playerHero))}
                     />
-                    <text
+
+                    {/* <text
                       x={columnIdxToX(i)}
                       y={timeToY(life.startTime, startTime, endTime)}
                       textAnchor="start"
                       fill={getColorFor(heroNameToNormalized(life.playerHero))}
                       fontSize={10}
                       dx={10}
-                      dy={2.5}>
-                      {life.startMessage}
-                    </text>
+                      dy={2.5}> */}
+                    <g
+                      transform={`translate(${columnIdxToX(i) + 10}, ${
+                        timeToY(life.startTime, startTime, endTime) - 13
+                      })`}>
+                      <foreignObject
+                        width="80"
+                        height="50"
+                        requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility">
+                        <p
+                          style={{
+                            color: getColorFor(
+                              heroNameToNormalized(life.playerHero),
+                            ),
+                            fontSize: 10,
+                            lineHeight: 0.9,
+                          }}>
+                          {life.startMessage}
+                        </p>
+                      </foreignObject>
+                      {/* </text> */}
+                    </g>
                     <line
                       x1={columnIdxToX(i) - 5}
                       y1={timeToY(life.endTime, startTime, endTime)}
---
+++ src/hooks/useMapTimes.ts
@@ -0,0 +1,87 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect, useState} from 'react';
+import {AlaSQLNode} from '../WombatDataFramework/DataTypes';
+import {useDataNodes} from './useData';
+
+const useMapTimes = (
+  mapId: number,
+): {
+  startTime: number;
+  endTime: number;
+}[] => {
+  const data = useDataNodes([
+    new AlaSQLNode(
+      'UseMapTimes_match_time_' + mapId,
+      `SELECT
+        match_start.matchTime as startTime,
+        match_end.matchTime as endTime
+      FROM ? AS match_start
+      JOIN
+      ? AS match_end
+      ON
+        match_start.mapId = match_end.mapId
+      WHERE
+        match_start.mapId = ${mapId}
+      `,
+      ['match_start_object_store', 'match_end_object_store'],
+    ),
+    new AlaSQLNode(
+      'UseMapTimes_round_times_' + mapId,
+      `SELECT
+        round_start.roundNumber,
+        round_start.matchTime as startTime,
+        round_end.matchTime as endTime
+      FROM ? AS round_end
+      JOIN
+      ? AS round_start
+      ON
+        round_start.mapId = round_end.mapId
+      WHERE
+        round_start.mapId = ${mapId}
+      ORDER BY
+        round_start.roundNumber
+      `,
+      ['round_end_object_store', 'round_start_object_store'],
+    ),
+  ]);
+
+  const matchTimes = data['UseMapTimes_match_time_' + mapId];
+  const roundTimes = data['UseMapTimes_round_times_' + mapId];
+
+  // zero index is the whole match, other indexes are the rounds
+  const [mapTimes, setMapTimes] = useState<
+    {
+      startTime: number;
+      endTime: number;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    if (!matchTimes || !roundTimes) {
+      return;
+    }
+
+    const mapTimes = [
+      {
+        startTime: matchTimes[0].startTime,
+        endTime: matchTimes[0].endTime,
+      },
+      ...roundTimes.map((round: any) => {
+        return {
+          startTime: round.startTime,
+          endTime: round.endTime,
+        };
+      }),
+    ];
+
+    setMapTimes(mapTimes);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [JSON.stringify(matchTimes), JSON.stringify(roundTimes)]);
+
+  console.log('mapTimes', mapTimes);
+
+  return mapTimes;
+};
+
+export default useMapTimes;
