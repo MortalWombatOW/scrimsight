c7724882865fc661ae23cf6e97de19834b03e27a
Andrew Gleeson <mortalwombat@scrimsight.com>
2024-07-11 16:29:59 -0700

    Merge branch 'master' of https://github.com/MortalWombatOW/scrimsight

--- src/hooks/useDeepEffect.ts
+++ src/hooks/useDeepEffect.ts
@@ -36,7 +36,7 @@ function objectDeepEquals<T extends object>(a: T, b: T): boolean {
 
 function deepEquals<T>(a: T, b: T): boolean {
   if (Array.isArray(a) && Array.isArray(b)) {
-    return arrayDeepEquals(a, b);
+    return arrayDeepEquals(a as T[], b as T[]);
   }
 
   if (typeof a === 'object' && typeof b === 'object') {
