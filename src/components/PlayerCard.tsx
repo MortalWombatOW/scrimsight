9a8ea62e50e8cca373f7dc01ea90623acf0b1593
Andrew Gleeson <mortalwombat@scrimsight.com>
2025-11-24 10:52:48 -0800

    fix: enable navigation to player details page

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -7,6 +7,8 @@ interface PlayerCardProps {
   heroes: string[];
   primaryStats: { value: string; label: string }[];
   secondaryStats?: { value: string; label: string }[];
+  linkUrl?: string;
+  linkText?: string;
 }
 
 export const PlayerCard = ({
@@ -15,17 +17,23 @@ export const PlayerCard = ({
   secondaryStats,
   teamNames,
   heroes,
+  linkUrl,
+  linkText,
 }: PlayerCardProps) => (
   <CardBase
     title={playerName}
     primaryStats={primaryStats}
     secondaryStats={secondaryStats}
+    linkUrl={linkUrl}
+    linkText={linkText}
     info={[
       <CardBaseFact
+        key="teams"
         value={listToNaturalLanguage(teamNames)}
         label={teamNames.length === 1 ? "Team" : "Teams"}
       />,
       <CardBaseFact
+        key="heroes"
         value={listToNaturalLanguage(heroes)}
         label={heroes.length === 1 ? "Hero" : "Heroes"}
       />,
